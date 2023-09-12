151c04514b5466b9611d01bec37ec3cc20b3c91c
Douglas R Miles <logicmoo@gmail.com>
2023-09-11 19:08:33 -0700

    OBO Parser release a little whitespace

--- .Attic/base/flybase_obo.pl
+++ .Attic/base/flybase_obo.pl
@@ -59,21 +59,23 @@ process_obo_stream(Stream, Type, Id) :-
 
 
 into_rest(Rest,RestChars,RestStr):-
-  string_chars(Str,Rest),
+  obo_string(Str,Rest),
   normalize_space(chars(RestChars),Str),
-  string_chars(RestStr,RestChars).
+  obo_string(RestStr,RestChars).
 
+obo_string(String,[C|Chars]):- var(String), C==' ',!, obo_string(String,Chars).
+obo_string(String,Chars):- string_chars(String,Chars).
 
 process_obo_chars( _, [e,n,d,'_',o,f,'_',f,i,l,e], _):-!.
 process_obo_chars( _, [], _) :- !.
 
 process_obo_chars( _, ['['|Chars], _):- append(Left,[']'],Chars),!,
-  must_det_ll(( atom_chars(Type,Left),!, nb_setval(obo_type,Type))).
+  must_det_ll(( symbol_chars(Type,Left),!, nb_setval(obo_type,Type))).
 
 process_obo_chars( Type, Chars, _):-
   get_key(Key,Chars,Rest),Key == id,
   into_rest(Rest,RestChars,_RestStr),
-  atom_chars(Id,RestChars), assert_OBO(id_type,Id,Type),
+  symbol_chars(Id,RestChars), assert_OBO(id_type,Id,Type),
   nb_setval(obo_id,Id),nb_setval(obo_type,Type).
 
 process_obo_chars( Type, Chars, Id):-
@@ -91,13 +93,13 @@ process_obo_rest_line(_Type,Id,Ref,_Chars,S):-
 process_obo_rest_line(Type,Id,Reln,Chars,_):-  Reln = relationship,!,
   must_det_ll((
    key_like_string(KeyLike,Chars,Rest),
-    atom_chars(Key,KeyLike),
+    symbol_chars(Key,KeyLike),
     into_rest(Rest,RestChars,RestStr),
     process_obo_rest_line(Type,Id,Key,RestChars,RestStr))).
 
 process_obo_rest_line(_Type,Id,Ref,Chars,_):-
     \+ (member(C,Chars),member(C,['!','[','"'])),
-    ( \+ member(' ',Chars)-> atom_chars(S,Chars);string_chars(S,Chars)),
+    ( \+ member(' ',Chars)-> symbol_chars(S,Chars);obo_string(S,Chars)),
     assert_OBO(Ref,Id,S),!.
 
 process_obo_rest_line(_Type,Id,is_a,Chars,Str):-
@@ -146,25 +148,25 @@ Would be parsed into the following Prolog terms:
 */
 
 
-get_key(Key)-->key_like_string(Chars),[':'],{atom_chars(Key,Chars)},!.
+get_key(Key)-->key_like_string(Chars),[':'],{symbol_chars(Key,Chars)},!.
 get_some_items(I)--> [' '],!,get_some_items(I).
 get_some_items(_,[],[]):-!.
 get_some_items([H|T])-->get_one_item(H),get_some_items(T). get_some_items([])-->[].
 get_one_item(I)--> [' '],!,get_one_item(I).
-get_one_item(quoted(Item))-->[x,s,d,':'],symbol_or_url(Chars),{atom_chars(Item,[x,s,d,':'|Chars])}.
-get_one_item(quoted(Item))-->[h,t,t,p],symbol_or_url(Chars),{string_chars(Item,[h,t,t,p|Chars])}.
-get_one_item(quoted(Item))-->[f,t,p],symbol_or_url(Chars),{string_chars(Item,[f,t,p|Chars])}.
-get_one_item(quoted(Item))-->['"'],string_until_end_quote(Chars),{string_chars(Item,Chars)}.
-get_one_item(named(Item))-->['!'],whs,named_like_string(Chars),{atom_chars(Item,Chars)}.
+get_one_item(quoted(Item))-->[x,s,d,':'],symbol_or_url(Chars),{symbol_chars(Item,[x,s,d,':'|Chars])}.
+get_one_item(quoted(Item))-->[h,t,t,p],symbol_or_url(Chars),{obo_string(Item,[h,t,t,p|Chars])}.
+get_one_item(quoted(Item))-->[f,t,p],symbol_or_url(Chars),{obo_string(Item,[f,t,p|Chars])}.
+get_one_item(quoted(Item))-->['"'],string_until_end_quote(Chars),{obo_string(Item,Chars)}.
+get_one_item(named(Item))-->['!'],whs,named_like_string(Chars),{symbol_chars(Item,Chars)}.
 get_one_item(bracketed(Items))-->['['],whs,items(Items),whs,[']'].
 get_one_item(bracketed(Items))-->['{'],whs,items(Items),whs,['}'].
-%get_one_item(item(Item))--> whs,key_like_string(Chars),whs,{Chars \==[], atom_chars(Item,Chars)}.
-get_one_item(keyword(Keyword))-->whs,id_like_string(Chars),{Chars\==[]},whs,{atom_chars(Keyword,Chars)}.
-get_one_item(text(Text))-->named_like_string(Chars),{string_chars(Text,Chars)}.
-get_one_item(text(Text),[H|T],[]):- ground([H|T]),string_chars(Text,[H|T]),!.
+%get_one_item(item(Item))--> whs,key_like_string(Chars),whs,{Chars \==[], symbol_chars(Item,Chars)}.
+get_one_item(keyword(Keyword))-->whs,id_like_string(Chars),{Chars\==[]},whs,{symbol_chars(Keyword,Chars)}.
+get_one_item(text(Text))-->named_like_string(Chars),{obo_string(Text,Chars)}.
+get_one_item(text(Text),[H|T],[]):- ground([H|T]),obo_string(Text,[H|T]),!.
 items([Item|Rest])-->item(Item),whs,[','],whs,items(Rest).
 items([Item])-->item(Item),!.
-item(Item)-->symbol_or_url(Chars),{Chars\==[],atom_chars(Item,Chars)}.
+item(Item)-->symbol_or_url(Chars),{Chars\==[],symbol_chars(Item,Chars)}.
 key_like_string([H|T])-->[H],{\+member(H,[':',' ','\t','\n'])},key_like_string(T).
 key_like_string([])-->[].
 id_like_string([H|T])-->[H],{\+member(H,['!',' ','\t','\n',',','[',']','{','}','"'])},id_like_string(T).
