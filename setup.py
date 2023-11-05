3c82c55e8a4291444b2079cc034ff2c1a799a15d
Douglas R Miles <logicmoo@gmail.com>
2023-11-05 14:24:28 -0800

    setup starting

--- setup.py
+++ setup.py
@@ -1,35 +1,38 @@
+from setuptools import setup, find_packages
 import io
 import sys
 import glob
 
-from setuptools import find_packages, setup
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-setup(name='metta_vspace',
-      version='0.1.1',
-      description='A MeTTa kernel for Jupyter that can use Python libraries',
+setup(
+	  name="vspace_metta",
+      version="0.1.1",
+      author="Douglas R. Miles",
+      author_email="logicmoo@gmail.com",
+      description="A MeTTa kernel for Jupyter that can use Python libraries",
       long_description=readme,
-      author='Douglas R. Miles',
-      author_email='logicmoo@gmail.com',
-      url="https://github.com/logicmoo/vspace_metta",
-      install_requires=["metakernel", "yasi"],
+      long_description_content_type="text/markdown",
+      url="https://github.com/logicmoo/vspace-metta",
+      
       packages=find_packages(include=["metta_vspace", "metta_vspace.*"]),
-      package_data={'metta_vspace': ["images/*.png", "modules/*.ss"]},
+      package_data={"metta_vspace": ["images/*.png", "modules/*.ss"]},
       platforms=["Any"],
       scripts = ["scripts/metta-jupyter-kernel", "scripts/metta-jupyter-kernel-debug"],
       data_files=[
-          ('share/jupyter/kernels/metta_vspace',
-           ['metta_vspace/kernel.json'] + glob.glob('metta_vspace/images/*.png')
+          ("share/jupyter/kernels/metta_vspace",
+           ["metta_vspace/kernel.json"] + glob.glob("metta_vspace/images/*.png")
           )
       ],
+      install_requires=["metakernel", "yasi"],
       classifiers = [
-          'Framework :: IPython',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: MeTTa',
-          'Topic :: System :: Shells',
+          "Framework :: IPython",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: MeTTa",
+          "Topic :: System :: Shells",
       ]
 )
