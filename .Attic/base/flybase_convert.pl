da1cf9b67ba4954d3080b277c09c4f659ed4ff36
Douglas R Miles <logicmoo@gmail.com>
2023-11-05 14:48:53 -0800

    vspace-metta/pyswip/flybase_induced_types.pl

---
+++ .Attic/base/flybase_convert.pl
@@ -0,0 +1,11 @@
+:- encoding(iso_latin_1).
+:- flush_output.
+:- setenv('RUST_BACKTRACE',full).
+
+
+:- ensure_loaded(swi_support).
+:- ensure_loaded(flybase_obo).
+:- ensure_loaded(flybase_json).
+:- ensure_loaded(flybase_main).
+
+
