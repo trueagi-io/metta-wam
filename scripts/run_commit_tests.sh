8e26040514747f2a9c1577c442546000aac1eb52
TeamSPoon <logicmoo@gmail.com>
2024-08-14 01:05:46 -0700

    resorts/SHARED.UNITS.PREV.md

--- scripts/run_commit_tests.sh
+++ scripts/run_commit_tests.sh
@@ -4,5 +4,5 @@
 # Replace the following lines with the actual commands to generate the input file.
 
 #echo "| ANTI-REGRESSION.BC-COMP.01 | PASS |(https://example.com/test-report) | (assertEqualToResult (add-atom &kb (: axiom (nums 2 3)))) | (()) | (()) |" > /tmp/SHARED.UNITS
-cat ./scripts/SHARED.UNITS.PREV.md > /tmp/SHARED.UNITS
+cat ./reports/SHARED.UNITS.PREV.md > /tmp/SHARED.UNITS
 # You can add more lines or commands to generate additional input data
