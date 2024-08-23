4185334cf564b3ab7d9648f873e5d4388d7e4f1d
Douglas R. Miles <logicmoo@gmail.com>
2024-08-23 12:52:59 -0700

    to run one test you have to use --test

--- scripts/run_commit_tests.sh
+++ scripts/run_commit_tests.sh
@@ -6,6 +6,6 @@ output=reports/tests_output/baseline-compat-$timestamp/
 
 # run the tests
 echo Running baseline_compat tests to $output
-#cat ./reports/SHARED.UNITS.PREV.md > /tmp/SHARED.UNITS
-cat /dev/null> /tmp/SHARED.UNITS
-mettalog --output=$output tests/baseline_compat/anti-regression/comma_is_not_special.metta
+cat ./reports/SHARED.UNITS.PREV.md > /tmp/SHARED.UNITS
+#cat /dev/null> /tmp/SHARED.UNITS
+#mettalog --output=$output --test --clean tests/baseline_compat/anti-regression/comma_is_not_special.metta
