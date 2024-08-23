fa04e7b1bd1e8e97190467e774f418eb8e8e68a7
AdrickTench <adricktench2018@u.northwestern.edu>
2024-08-23 11:11:41 -0500

    try running tests with venv

--- scripts/run_commit_tests.sh
+++ scripts/run_commit_tests.sh
@@ -5,5 +5,6 @@ timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
 output=reports/tests_output/baseline-compat-$timestamp/
 
 # run the tests
+. ./scripts/ensure_venv
 echo Running baseline_compat tests to $output
 mettalog --output=$output tests/baseline_compat #> /dev/null 2>&1
