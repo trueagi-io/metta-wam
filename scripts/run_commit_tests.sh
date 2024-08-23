79b63537474b4f8f64ac9d7cb0dc406cb8b067c7
AdrickTench <adricktench2018@u.northwestern.edu>
2024-08-23 11:03:56 -0500

    separate install and run tests step, show test output (for now)

--- scripts/run_commit_tests.sh
+++ scripts/run_commit_tests.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -e # stop if any step fails
-
-# install mettalog
-chmod +x INSTALL.sh  # Make sure the script is executable
-. ./INSTALL.sh --easy
 
 # generate the output directory with timestamp
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
@@ -11,4 +6,4 @@ output=reports/tests_output/baseline-compat-$timestamp/
 
 # run the tests
 echo Running baseline_compat tests to $output
-mettalog --output=$output tests/baseline_compat > /dev/null 2>&1
+mettalog --output=$output tests/baseline_compat #> /dev/null 2>&1
