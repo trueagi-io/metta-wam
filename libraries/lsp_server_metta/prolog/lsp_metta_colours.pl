b1f506e7ed6931444f6ac45ac87fed25d4357696
Roy Ward <roy@trurl.niesward.home>
2024-08-31 19:53:40 -0700

    zeroth cut at lsp metta server package

---
+++ libraries/lsp_server_metta/prolog/lsp_metta_colours.pl
@@ -0,0 +1,41 @@
+:- module(lsp_metta_colours, [
+                        token_types/1,
+                        token_modifiers/1]).
+
+% these are Prolog token types/modifiers. Need to change to meTTa.
+
+token_types([namespace,
+             type,
+             class,
+             enum,
+             interface,
+             struct,
+             typeParameter,
+             parameter,
+             variable,
+             property,
+             enumMember,
+             event,
+             function,
+             member,
+             macro,
+             keyword,
+             modifier,
+             comment,
+             string,
+             number,
+             regexp,
+             operator
+            ]).
+token_modifiers([declaration,
+                 definition,
+                 readonly,
+                 static,
+                 deprecated,
+                 abstract,
+                 async,
+                 modification,
+                 documentation,
+                 defaultLibrary
+                ]).
+
