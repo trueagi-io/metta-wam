46956669dc4e68f3d8ee4c58804523997128b3b3
logicmoo <logicmoo@gmail.com>
2024-09-14 08:36:50 -0700

    towards https://github.com/trueagi-io/metta-wam/issues/11 and https://github.com/trueagi-io/metta-wam/issues/12 and https://github.com/trueagi-io/metta-wam/issues/7

---
+++ python/hyperon/stdlib.py
@@ -0,0 +1,191 @@
+import re
+
+from .atoms import ExpressionAtom, E, GroundedAtom, OperationAtom, ValueAtom, NoReduceError, AtomType, MatchableObject, \
+    G, S, Atoms
+from .base import Tokenizer, SExprParser
+from .ext import register_atoms, register_tokens
+import hyperonpy as hp
+
+class Char:
+    """Emulate Char type as in a traditional FPL"""
+    def __init__(self, char):
+        if len(char) != 1:
+            raise ValueError("A Char object must be initialized with a single character.")
+        self.char = char
+
+    def __str__(self):
+        return self.char
+
+    def __repr__(self):
+        return f"'{self.char}'"
+
+    def __eq__(self, other):
+        if isinstance(other, Char):
+            return self.char == other.char
+        return False
+
+@register_atoms
+def arithm_ops():
+    subAtom = OperationAtom('-', lambda a, b: a - b, ['Number', 'Number', 'Number'])
+    mulAtom = OperationAtom('*', lambda a, b: a * b, ['Number', 'Number', 'Number'])
+    addAtom = OperationAtom('+', lambda a, b: a + b, ['Number', 'Number', 'Number'])
+    divAtom = OperationAtom('/', lambda a, b: a / b, ['Number', 'Number', 'Number'])
+    modAtom = OperationAtom('%', lambda a, b: a % b, ['Number', 'Number', 'Number'])
+    return {
+        r"\+": addAtom,
+        r"-": subAtom,
+        r"\*": mulAtom,
+        r"/": divAtom,
+        r"%": modAtom
+    }
+
+@register_atoms
+def bool_ops():
+    equalAtom = OperationAtom('==', lambda a, b: [ValueAtom(a == b, 'Bool')],
+                              ['$t', '$t', 'Bool'], unwrap=False)
+    greaterAtom = OperationAtom('>', lambda a, b: a > b, ['Number', 'Number', 'Bool'])
+    lessAtom = OperationAtom('<', lambda a, b: a < b, ['Number', 'Number', 'Bool'])
+    greaterEqAtom = OperationAtom('>=', lambda a, b: a >= b, ['Number', 'Number', 'Bool'])
+    lessEqAtom = OperationAtom('<=', lambda a, b: a <= b, ['Number', 'Number', 'Bool'])
+    orAtom = OperationAtom('or', lambda a, b: a or b, ['Bool', 'Bool', 'Bool'])
+    andAtom = OperationAtom('and', lambda a, b: a and b, ['Bool', 'Bool', 'Bool'])
+    notAtom = OperationAtom('not', lambda a: not a, ['Bool', 'Bool'])
+    return {
+        r"==": equalAtom,
+        r"<": lessAtom,
+        r">": greaterAtom,
+        r"<=": lessEqAtom,
+        r">=": greaterEqAtom,
+        r"or": orAtom,
+        r"and": andAtom,
+        r"not": notAtom
+    }
+
+def get_string_value(value) -> str:
+    if not isinstance(value, str):
+        value = repr(value)
+    if len(value) > 2 and ("\"" == value[0]) and ("\"" == value[-1]):
+        return value[1:-1]
+    return value
+
+class RegexMatchableObject(MatchableObject):
+    ''' To match atoms with regular expressions'''
+
+    def __init__(self, content, id=None):
+        super().__init__(content, id)
+
+        self.content = self.content.replace("[[", "(").replace("]]", ")").replace("~", " ")
+
+    def match_text(self, text, regexpr):
+        return re.search(pattern=regexpr, string=text.strip(), flags=re.IGNORECASE)
+
+    def match_(self, atom):
+        pattern = self.content
+        text = get_string_value(atom)
+        text = ' '.join([x.strip() for x in text.split()])
+        if pattern.startswith("regex:"):
+            pattern = get_string_value(pattern[6:])
+            matched = self.match_text(text, pattern)
+            if matched is not None:
+                return [{"matched_pattern": S(pattern)}]
+        return []
+
+
+
+
+@register_atoms
+def text_ops():
+    """Add text operators
+
+    repr: convert Atom to string.
+    parse: convert String to Atom.
+    stringToChars: convert String to tuple of Char.
+    charsToString: convert tuple of Char to String.
+
+    see test_stdlib.py for examples.
+
+    """
+
+    reprAtom = OperationAtom('repr', lambda a: [ValueAtom(repr(a))],
+                             ['Atom', 'String'], unwrap=False)
+    parseAtom = OperationAtom('parse', lambda s: [ValueAtom(SExprParser(str(s)[1:-1]).parse(Tokenizer()))],
+                              ['String', 'Atom'], unwrap=False)
+    stringToCharsAtom = OperationAtom('stringToChars', lambda s: [ValueAtom(E(*[ValueAtom(Char(c)) for c in str(s)[1:-1]]))],
+                                      ['String', 'Atom'], unwrap=False)
+    charsToStringAtom = OperationAtom('charsToString', lambda a: [ValueAtom("".join([str(c)[1:-1] for c in a.get_children()]))],
+                                      ['Atom', 'String'], unwrap=False)
+    return {
+        r"repr": reprAtom,
+        r"parse": parseAtom,
+        r"stringToChars": stringToCharsAtom,
+        r"charsToString": charsToStringAtom
+    }
+
+@register_tokens
+def type_tokens():
+    return {
+        r"[-+]?\d+" : lambda token: ValueAtom(int(token), 'Number'),
+        r"[-+]?\d+(\.\d+)": lambda token: ValueAtom(float(token), 'Number'),
+        r"[-+]?\d+(\.\d+)?e[-+]?\d+": lambda token: ValueAtom(float(token), 'Number'),
+        "\"[^\"]*\"": lambda token: ValueAtom(str(token[1:-1]), 'String'),
+        "\'[^\']\'": lambda token: ValueAtom(Char(token[1]), 'Char'),
+        r"True|False": lambda token: ValueAtom(token == 'True', 'Bool'),
+        r'regex:"[^"]*"': lambda token: G(RegexMatchableObject(token),  AtomType.UNDEFINED)
+    }
+
+@register_tokens
+def call_atom():
+    def newCallAtom(token):
+        # NOTE: we could use "call" as a plain symbol (insted of "call:...")
+        #       with the method name as the parameter of call_atom_op
+        #       (but this parameter should be unwrapped)
+        # "call:..." is an interesting example of families of tokens for ops, though
+        return OperationAtom(
+                    token,
+                    lambda obj, *args: call_atom_op(obj, token[5:], *args),
+                    unwrap=False)
+
+    def call_atom_op(atom, method_str, *args):
+        if not isinstance(atom, GroundedAtom):
+            # raise RuntimeError("call:" + method_str + " expects Python grounded atom")
+            raise NoReduceError()
+        obj = atom.get_object().value
+        method = getattr(obj, method_str)
+        result = method(*args)
+        if result is None:
+            return []
+        # Fixme? getting results from call_atom raises some issues but convenient.
+        # Running example is call:... &self (or another imported space)
+        # However if we need to wrap the result into GroundedAtom, we don't know
+        # its type. Also, if the method returns list, we can wrap it as whole or
+        # can interpret it as multiple results.
+        # Here, we don't wrap the list as whole, but wrap its elements even they
+        # are atoms, for get_atoms to work nicely (wrapped list is not printed
+        # nicely, while not wrapping atoms results in their further reduction)
+        # This functionality can be improved/changed based on other more
+        # important examples (e.g. dealing with DNN models) in the future,
+        # while the core functions like &self.get_atoms can be dealt with
+        # separately
+        if not isinstance(result, list):
+            result = [result]
+        result = [ValueAtom(r) for r in result]
+        #result = [r if isinstance(r, Atom) else ValueAtom(r) for r in result]
+        return result
+
+    return {
+        r"call:[^\s]+": newCallAtom
+    }
+
+@register_atoms
+def load_ascii():
+    def load_ascii_atom(space, name):
+        space_obj = space.get_object()
+        hp.load_ascii(name.get_name(), space_obj.cspace)
+        #['Space', 'Symbol', 'Unit'], 
+        return [Atoms.UNIT]
+
+    loadAtom = OperationAtom('load-ascii', load_ascii_atom,
+                             unwrap=False)
+    return {
+        r"load-ascii": loadAtom
+    }
---
+++ python/tests/test_grounded_type.py
@@ -0,0 +1,160 @@
+import unittest
+
+from hyperon import *
+import hyperonpy as hp
+
+class GroundedTypeTest(unittest.TestCase):
+
+    def test_apply_type(self):
+        metta = MeTTa(env_builder=Environment.test_env())
+        self.assertEqual(
+            metta.parse_single("+").get_grounded_type(),
+            metta.parse_single("*").get_grounded_type())
+        self.assertEqual(
+            metta.run("!(+ (* 1 4) 2)")[0][0].get_grounded_type(),
+            metta.parse_single("0").get_grounded_type())
+        self.assertEqual(
+            metta.run("!(or True False)")[0][0].get_grounded_type(),
+            metta.parse_single("False").get_grounded_type())
+        self.assertEqual(
+            metta.run("!(> (* 2 2) 1)")[0][0].get_grounded_type(),
+            metta.run("!(or True True)")[0][0].get_grounded_type())
+        metta.register_atom("untyped", ValueAtom(None))
+        metta.register_atom("untop", OperationAtom("untop", lambda: None))
+        self.assertEqual(
+            metta.run("!(untop)")[0][0],
+            metta.parse_single("()"))
+        self.assertEqual(
+            metta.run("!(untop)")[0][0].get_grounded_type(),
+            metta.parse_single("untyped").get_grounded_type())
+        self.assertNotEqual(
+            metta.run("!(untop)")[0][0].get_grounded_type(),
+            metta.run("!(+ 1 1)")[0][0].get_grounded_type())
+        self.assertNotEqual(
+            metta.run("!(> 1 1)")[0][0].get_grounded_type(),
+            metta.run("!(+ 1 1)")[0][0].get_grounded_type())
+
+    def test_higher_func(self):
+        metta = MeTTa(env_builder=Environment.test_env())
+        metta.register_atom(
+            r"curry_num",
+            OperationAtom(
+                "curry_num",
+                lambda op, x: [OperationAtom(
+                    'lmd',
+                    lambda y: op.get_object().op(x.get_object().value, y),
+                    ['Number', 'Number'])],
+                #FIXME: interpreter refuses to execute typed curry_num
+                #[['Number', 'Number', 'Number'], 'Number', ['Number', 'Number']],
+                unwrap=False))
+        self.assertEqual(metta.run("!((curry_num + 1) 2)"),
+                         metta.run("! 3"))
+
+    def test_meta_types(self):
+        metta = MeTTa(env_builder=Environment.test_env())
+        ### Basic functional types
+        metta.register_atom(r"id_num", OperationAtom("id_num", lambda x: x, ['Number', 'Number']))
+        metta.register_atom(r"as_int", OperationAtom("as_int", lambda x: x, ['Number', 'Int']))
+        v1 = metta.run("!(id_num (+ 2 2))")[0]
+        v2 = metta.run("! 4")[0]
+        v3 = metta.run("!(as_int (+ 2 2))")[0]
+        self.assertEqual(v1, v2)
+        self.assertEqual(v1[0].get_grounded_type(), v2[0].get_grounded_type())
+        self.assertNotEqual(v1[0].get_grounded_type(), v3[0].get_grounded_type())
+        # Untyped symbols don't cause type error, but the expression is not reduced
+        self.assertEqual(metta.run("!(id_num untyp)"), [metta.parse_all("(id_num untyp)")])
+        # Typed symbols cause type error when evaluated
+        metta.run("(: myAtom myType)")
+        self.assertEqual(metta.run('''
+            !(id_num myAtom)
+            !(id_num False)
+            '''),
+            [[E(S('Error'), S('myAtom'), S('BadType'))]])
+        ### Grounded functions over Atom
+        ### (should use unwrap=False to deal with non-grounded atoms)
+        # All grounded and ungrounded, typed and untyped symbols should be processed
+        metta.register_atom(r"id_atom", OperationAtom("id_atom",
+            lambda x: [x], [AtomType.ATOM, AtomType.ATOM], unwrap=False))
+        self.assertEqual(metta.run('''
+            !(id_atom 1)
+            !(id_atom myAtom)
+            !(id_atom untyp)
+            ''', flat=True),
+            metta.parse_all('''
+            1
+            myAtom
+            untyp
+            '''))
+        # FIXME: why does it get reduced?
+        # self.assertEqual(metta.run("!(id_atom (+ 1 1))"), [metta.parse_all("(+ 1 1)")])
+        ### Polymorphic without unwrapping
+        # Nothing is done with `$t` on the Grounded side, but we check that:
+        # - the argument has really a variable type
+        # - the interpreter doesn't reject to process this grounded function
+        metta.register_atom(r"id_poly_w", OperationAtom("id_poly_w", lambda x: [x], ['$t', '$t'], unwrap=False))
+        self.assertEqual(metta.run('''
+            !(id_poly_w 1)
+            !(id_poly_w myAtom)
+            !(id_poly_w untyp)
+            !(id_poly_w (+ 1 1))
+            !(+ 1 (id_poly_w 2))
+            ''', flat=True),
+            metta.parse_all('''
+             1
+             myAtom
+             untyp
+             2
+             3
+            ''')
+        )
+        ### Polymorphic with unwrapping
+        # TODO: automatic unwrapping of arguments of grounded polymorphic function
+        #       is not supported on the Python side
+        metta.register_atom(r"id_poly_u", OperationAtom("id_poly_u", lambda x: x, ['$t', '$t']))
+        ### Undefined arguments
+        # It is a bad idea to have an undefined result with automatic wrapping, but it's ok here
+        metta.register_atom(r"id_undef", OperationAtom("id_undef", lambda x: x, [AtomType.UNDEFINED, AtomType.UNDEFINED]))
+        self.assertEqual(metta.run('''
+            !(id_undef 1)
+            !(id_undef False)
+            !(id_undef (+ 1 1))
+            ''', flat=True),
+            metta.parse_all("1 False 2"))
+        # This will not be reduced, because unwrapping expects a grounded atom
+        self.assertEqual(metta.run('''
+            !(id_undef myAtom)
+            !(id_undef untyp)
+            ''', flat=True),
+            metta.parse_all('''
+             (id_undef myAtom)
+             (id_undef untyp)
+            ''')
+        )
+
+    # TODO: For now OperationAtom without type has Undefined type. We discussed
+    # that it would be nice to have something like *args in the future. The
+    # type of untyped operations is yet another example showing this can be
+    # useful. So, the type of untyped operations might become something like
+    # (-> *Undefined Undefined) in the future.
+    @unittest.skip("Behavior to be defined")
+    def test_undefined_operation_type(self):
+        metta = MeTTa(env_builder=Environment.test_env())
+        metta.register_atom("untyped", ValueAtom(None))
+        metta.register_atom("untop", OperationAtom("untop", lambda: None))
+        self.assertNotEqual(metta.parse_single("untop").get_grounded_type(),
+                metta.parse_single("untyped").get_grounded_type())
+
+    def test_number_conversion(self):
+        num = Atom._from_catom(hp.number_into_gnd(123))
+        self.assertEqual(type(num), GroundedAtom)
+        self.assertEqual(hp.gnd_get_int(num.catom), 123)
+
+        num = Atom._from_catom(hp.number_into_gnd(123.456))
+        self.assertEqual(type(num), GroundedAtom)
+        self.assertTrue(abs(hp.gnd_get_float(num.catom) - 123.456) < 0.0001)
+
+        self.assertEqual(hp.gnd_get_int(S("sym").catom), None)
+        self.assertEqual(hp.gnd_get_float(S("sym").catom), None)
+
+if __name__ == "__main__":
+    unittest.main()
