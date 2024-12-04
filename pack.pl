263ebc3eb439c080f7e8e185ac7ac5687a05c443
logicmoo <logicmoo@gmail.com>
2024-12-04 12:24:25 -0800

    the pack.pl

---
+++ pack.pl
@@ -0,0 +1,12 @@
+name(metta_lang).
+version('0.0.1').
+title('Implemen taion of the meTTa language by SingularityNet.IO').
+keywords([local_scoping,utilities,debugging,loop_checking]).
+author( 'LOGICMOO CO', 'https://logicmoo.org' ).
+packager( 'LOGICMOO CO', 'https://logicmoo.org' ).
+maintainer( 'LOGICMOO CO', 'https://logicmoo.org' ).
+home( 'https://github.com/trueagi-io/metta-wam' ).
+download( 'https://github.com/trueagi-io/metta-wam/release/*.zip' ).
+%requires(predicate_streams).
+% requires(dictoo).
+autoload(true).
