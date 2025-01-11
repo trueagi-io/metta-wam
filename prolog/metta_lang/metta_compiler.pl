9d7e3aab307c78ed3d584431fae99a5cb6881f19
Roy Ward <roy@trurl.niesward.home>
2025-01-11 15:55:04 -0800

    x

--- prolog/metta_lang/metta_compiler.pl
+++ prolog/metta_lang/metta_compiler.pl
@@ -477,6 +477,7 @@ transpile_eval(Convert0,LiConverted,PrologCode) :-
    ).
 
 arg_properties_widen(L,L,L) :- !.
+arg_properties_widen(x(_,eager),x(_,eager),x(noeval,eager)) :- !.
 arg_properties_widen(_,_,x(noeval,lazy)).
 
 combine_transpiler_cause_store_aux(ArgsN-RetN,Args0-Ret0,Args1-Ret1) :-
@@ -562,7 +563,6 @@ compile_for_assert(HeadIsIn, AsBodyFnIn, Converted) :-
    must_det_lls((
       %(FnName='facF' -> trace ; true),
       %trace(f2p/6),
-      trace,
       Converted = (HeadC :- NextBodyC),  % Create a rule with Head as the converted AsFunction and NextBody as the converted AsBodyFn
       get_operator_typedef_props(_,FnName,LenArgs,Types0,RetType0),
       maplist(arg_eval_props,Types0,TypeProps),
@@ -1294,7 +1294,7 @@ f2p(HeadIs,LazyVars,_,_,EvalArgs,Convert,_,_):-
 
 % eager -> eager, lazy -> lazy
 lazy_impedance_match(x(_,L),x(doeval,L),ValE,CodeE,_ValN,_CodeN,ValE,CodeE).
-lazy_impedance_match(x(_,L),x(noeval,L),_ValE,_CodeE,ValN,CodeN,ValN,CodeN).
+lazy_impedance_match(x(_,L),x(noeval,L),ValE,CodeE,_ValN,_CodeN,ValE,CodeE).
 % lazy -> eager
 lazy_impedance_match(x(_,lazy),x(doeval,eager),ValE,CodeE,_ValN,_CodeN,RetResult,Code) :- append(CodeE,[[native(as_p1_exec),ValE,RetResult]],Code).
 lazy_impedance_match(x(_,lazy),x(noeval,eager),_ValE,_CodeE,ValN,CodeN,RetResult,Code) :- append(CodeN,[[native(as_p1_expr),ValN,RetResult]],Code).
