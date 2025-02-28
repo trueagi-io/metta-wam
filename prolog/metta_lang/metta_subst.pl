ea35ef76b388f9baa489ea89ba3117eaac90efe0
logicmoo <logicmoo@gmail.com>
2025-02-28 00:35:46 -0800

    Add `find_missing_cuts/0` directive at the end of `metta_subst.pl` to systematically identify nondeterminism issues

--- prolog/metta_lang/metta_subst.pl
+++ prolog/metta_lang/metta_subst.pl
@@ -932,4 +932,4 @@ setof_subst(Depth,Self,X,S):- !,findall(E,subst_args(Eq,RetType,Depth,Self,X,E),
 %setof_subst(Depth,Self,X,S):- setof(E,subst_args(Eq,RetType,Depth,Self,X,E),S)*->true;S=[].
 
 
-
+:- find_missing_cuts.
