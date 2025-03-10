7b53d16837e47d129881831a3bb6a0031ae5330d
James N. V. Cash <james.cash@occasionallycogent.com>
2025-03-10 12:04:09 -0400

    [lsp] Use split_string/4 to trim whitespace instead of bespoke impl

--- libraries/lsp_server_metta/prolog/lsp_metta_hover.pl
+++ libraries/lsp_server_metta/prolog/lsp_metta_hover.pl
@@ -83,29 +83,9 @@ trim_white_lines(Input, Trimmed) :-
   % Convert Input to a string if it’s an atom
   (atom(Input) -> atom_string(Input, Str); Str = Input),
   % Trim leading and trailing whitespace, newlines, and carriage returns
-  trim_leading(Str, TrimmedStr1),
-  trim_trailing(TrimmedStr1, TrimmedStr),
+  split_string(Str, "", "\n\r\s\t", [TrimmedStr]),
   % Convert back to atom if original input was an atom
   (atom(Input) -> atom_string(Trimmed, TrimmedStr); Trimmed = TrimmedStr).
-
-% Helper predicate to trim leading whitespace, newlines, and carriage returns
-trim_leading(Str, TrimmedStr) :- sub_string(Str, 0, 1, _, Start), memberchk(Start, ["\n", "\r"]), !,
-  sub_string(Str, 1, _, 0, Str1), !, trim_leading(Str1, TrimmedStr).
-trim_leading(Str, Str).  % Base case: no leading newline, return unchanged
-
-% Helper predicate to trim trailing whitespace, newlines, and carriage returns
-trim_trailing(Str, TrimmedStr) :-
-  sub_string(Str, _, 1, 0, LastChar),
-  ( LastChar = '\n' ; LastChar = '\r' ; LastChar = ' ' ), !,
-  sub_string(Str, 0, _, 1, Str1),
-  trim_trailing(Str1, TrimmedStr).
-trim_trailing(Str, Str).  % Base case: no trailing newline, return unchanged
-
-
-% string_contains(String, Substring): true if Substring is contained within String
-string_contains(String, Substring) :-
-  sub_string(String, _, _, _, Substring).
-
 
 lsp_hooks:hover_string(Path, Loc, Term, Arity, S):-
        lsp_call_metta(['hook-hover-string', Path, Loc, Term, Arity], Out), string(Out),  S = Out, !. % wots(S, write_src(Out)).
