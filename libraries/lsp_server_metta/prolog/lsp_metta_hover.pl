7f815bdcb3a0c443a44cb0f173b0b0fd914001ec
James N. V. Cash <james.cash@occasionallycogent.com>
2025-03-11 13:20:55 -0400

    [lsp] Formatting

--- libraries/lsp_server_metta/prolog/lsp_metta_hover.pl
+++ libraries/lsp_server_metta/prolog/lsp_metta_hover.pl
@@ -76,7 +76,7 @@ trim_white_lines(Input, Trimmed) :-
   (atom(Input) -> atom_string(Trimmed, TrimmedStr); Trimmed = TrimmedStr).
 
 lsp_hooks:hover_string(Path, Loc, Term, Arity, S):-
-       lsp_call_metta(['hook-hover-string', Path, Loc, Term, Arity], Out), string(Out),  S = Out, !. % wots(S, write_src(Out)).
+    lsp_call_metta(['hook-hover-string', Path, Loc, Term, Arity], Out), string(Out),  S = Out, !. % wots(S, write_src(Out)).
 
 lsp_hooks:hover_print(Path, Loc, Term, Arity):-
     lsp_call_metta(['hook-hover-print', Path, Loc, Term, Arity], Res),
