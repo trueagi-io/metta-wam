57bc305f9e07fe7a7d42d55333d8052e1aa15611
logicmoo <logicmoo@gmail.com>
2025-06-24 05:23:28 -0700

    prolog/metta_lang/metta_compiler_douglas.pl

--- prolog/metta_rt.pl
+++ prolog/metta_rt.pl
@@ -1,6 +1,9 @@
 
+:- set_prolog_flag(mettalog_rt,true).
 
-:- (prolog_load_context(directory, Value);Value='.'), absolute_file_name('../libraries/',Dir,[relative_to(Value)]),
+:- (prolog_load_context(directory, Value);Value='.'), absolute_file_name('../libraries/',Dir,[relative_to(Value),file_type(directory)]),
+    absolute_file_name('.',Here,[relative_to(Value),file_type(directory)]),
+    asserta(metta_rt_src_file(Here)),
     atom_concat(Dir,'predicate_streams',PS),
     atom_concat(Dir,'logicmoo_utils',LU),
     atom_concat(Dir,'lsp_server_metta',LSP),
@@ -9,5 +12,18 @@
     pack_attach(LU,[duplicate(replace),search(first)]),
     pack_attach(LSP,[duplicate(replace),search(first)]).
 
-%:- ensure_loaded(library(metta_lang/metta_interp)).
+
+interp_src_dir(Dir):- getenv('INTERP_SRC_DIR',Dir),exists_directory(Dir),!.
+interp_src_dir(Dir):- metta_rt_src_file(Here), absolute_file_name('metta_lang',Dir,[relative_to(Here),file_type(directory)]).
+
+normal_IO:- stream_property(I,file_no(0)),
+   stream_property(O,file_no(1)),
+   stream_property(E,file_no(2)),
+   set_system_IO(I,O,E),
+   set_prolog_IO(I,O,E),!.
+
+
+ii:- interp_src_dir(Dir),
+   absolute_file_name('metta_interp',File,[relative_to(Dir)]),
+   load_files(File, [qcompile(auto)]).
 
